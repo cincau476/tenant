043b60cae40ec64d221049dd4e3664d99c8957a1
virrel <virrelj@gmail.com>
2025-12-31 13:23:59 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* Bungkus App di sini */}
+    <BrowserRouter basename="/tenant"> {/* Bungkus App di sini */}
       <App />
     </BrowserRouter>
   </StrictMode>,
